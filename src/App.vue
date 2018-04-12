0023c9cad903ef28fa0e6be5a572970fd3fede64
DouNingNing <doudou_0226@sina.com>
2018-04-12 15:09:50 +0800

    First Submit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <m-header></m-header>
+    <tab></tab>
+    <keep-alive><!--keep-alive可以把dom缓存在内存中，不会重新请求数据-->
+      <router-view></router-view>
+    </keep-alive>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  import MHeader from 'components/m-header/m-header'
+  import Tab from 'components/tab/tab'
+
+  export default {
+    components: {
+      MHeader,
+      Tab
+    }
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+</style>
